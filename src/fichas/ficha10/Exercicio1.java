1030ebfa8201abd5e8c161002be189e5142913f1
Milkenm <miguel.campos.johnny@gmail.com>
2019-10-11 16:06:31 +0100

    F10 E1

---
+++ src/fichas/ficha10/Exercicio1.java
@@ -0,0 +1,25 @@
+package fichas.ficha10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercicio1
+{
+    public static void main(String[] args)
+    {
+	List<String> list = new ArrayList<>();
+
+	for (int i = 1; i <= 10; i++) list.add("Pessoa " + i);
+
+	Object resultado = Pesquisa(new ArrayList<Object>(list), "Pessoa 10");
+
+	if (resultado != null) System.out.println("O valor foi encontrado!");
+	else System.out.println("O valor não foi encontrado...");
+    }
+
+    private static Object Pesquisa(ArrayList<Object> list, Object valor)
+    {
+	if (list.contains(valor)) return valor;
+	return null;
+    }
+}
