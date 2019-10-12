485d5b513402099e9ece30a1a9bda4a664402096
Milkenm <miguel.campos.johnny@gmail.com>
2019-10-12 21:00:28 +0100

    Fix tabs

--- src/fichas/ficha10/Exercicio2.java
+++ src/fichas/ficha10/Exercicio2.java
@@ -7,14 +7,14 @@ public class Exercicio2
 {
     public static void main(String[] args)
     {
-	ArrayList<Integer> list = new ArrayList<>();
+		ArrayList<Integer> list = new ArrayList<>();
 
-	list.add(3);
-	list.add(7);
+		list.add(3);
+		list.add(7);
 
-	System.out.println("Valores iniciais: " + list.toString());
-	list = InsertInPlace(list, 2);
-	System.out.println("Valores finais: " + list.toString());
+		System.out.println("Valores iniciais: " + list.toString());
+		list = InsertInPlace(list, 2);
+		System.out.println("Valores finais: " + list.toString());
     }
 
     private static ArrayList<Integer> InsertInPlace(ArrayList<Integer> list, int inserir)
@@ -22,25 +22,25 @@ public class Exercicio2
         ArrayList<Integer> listord = new ArrayList<>();
         boolean inserido = false;
 
-	for (int i = 0; i < list.size(); i++)
-	{
-	    System.out.println(i);
-	    System.out.println(list.size());
+		for (int i = 0; i < list.size(); i++)
+		{
+			System.out.println(i);
+			System.out.println(list.size());
 
-	    if (inserir < list.get(i) && inserido == false)
-	    {
-		listord.add(inserir);
-		inserido = true;
-	    }
-	    else if (inserir > list.get(i) && i + 1 == list.size())
-	    {
-		listord.add(list.get(i));
-		listord.add(inserir);
-		break;
-	    }
-	    listord.add(list.get(i));
-	}
+			if (inserir < list.get(i) && inserido == false)
+			{
+				listord.add(inserir);
+				inserido = true;
+			}
+			else if (inserir > list.get(i) && i + 1 == list.size())
+			{
+				listord.add(list.get(i));
+				listord.add(inserir);
+				break;
+			}
+			listord.add(list.get(i));
+		}
 
-	return listord;
+		return listord;
     }
 }
