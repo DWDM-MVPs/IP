f2987996fca27d0e1ceb7921c6750e8980ca7b97
Milkenm <miguel.campos.johnny@gmail.com>
2019-10-16 17:35:42 +0100

    F11 E4

---
+++ src/fichas/ficha11/Exercicio4.java
@@ -0,0 +1,25 @@
+package fichas.ficha11;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Exercicio4
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> listnums = new ArrayList<>();
+        for (int i = 0; i < 10; i++) listnums.add(new Random().nextInt(1000));
+	for (int i = 0; i < 10; i++) System.out.println(listnums.get(i));
+
+	    DescrobrirMaior(listnums, 0);
+        System.out.println(maior);
+    }
+
+    static int maior;
+    private static void DescrobrirMaior(ArrayList<Integer> list, int index)
+    {
+        if (list.get(index) > maior) maior = list.get(index);
+	index += 1;
+        if (index < list.size()) DescrobrirMaior(list, index);
+    }
+}
