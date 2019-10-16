c6cc5ab9535e22f656f6d0c892f5b3f959166a70
Milkenm <miguel.campos.johnny@gmail.com>
2019-10-16 17:06:45 +0100

    F11 E3

---
+++ src/fichas/ficha11/Exercicio3.java
@@ -0,0 +1,17 @@
+package fichas.ficha11;
+
+public class Exercicio3
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Número de algarismos: " + NumAlgarismos(670));
+    }
+
+    static int algs, pos;
+    private static int NumAlgarismos(int num)
+    {
+        algs += 1;
+        if (algs < String.valueOf(num).length()) NumAlgarismos(num);
+        return algs;
+    }
+}
