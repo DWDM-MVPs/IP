4ffa389ec994e3787037a062cc32aea8c3bd2f0a
Milkenm <miguel.campos.johnny@gmail.com>
2019-10-16 15:19:09 +0100

    Comentário todo bonitinho

--- src/exemplos/Exemplo6.java
+++ src/exemplos/Exemplo6.java
@@ -11,6 +11,9 @@ public class Exemplo6
 
     static void InverterSeq(String texto)
     {
+        // O código espera que a função no 'if' imprima a mensagem, o que faz com que a primeira call seja a última
+        // a ser imprimida e a última a ser a primeira a ser imprimida.
+
         char letra = texto.charAt(0);
         if (letra != ' ') InverterSeq(texto.substring(1));
         System.out.println(letra);
